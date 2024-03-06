e84704fecd16e792bf65a79b3776101500183ff6
eliteappgit <elite.app.22@gmail.com>
2024-03-06 17:30:21 +0530

    primitive non primitive understanding

---
+++ Types_Values_Variables/primitive_non_primitive.js
@@ -0,0 +1,23 @@
+/**
+ * Js divided i two types Primitive and not Primitive, You can also say call by value call by reffrence.
+ */
+
+// Primitive Data types :- stores into stack and uses copy.
+/**
+ * Number
+ * String
+ * Boolean
+ * Float
+ * Bigint
+ * Undefined
+ * Null
+ * Symbol
+ */
+
+
+// Non Primitive Datatypes :- stores in heap and reffer an address of heap memory. 
+
+/**
+ * Object
+ * Array
+ */
